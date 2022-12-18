f60753a736faf220601166af212111cac656fdc5
doomout <winquake3@hanmail.net>
2022-12-18 13:56:02 +0900

    아웃렛 변수와 액션 함수 추가

--- ImageView/ViewController.swift
+++ ImageView/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imgView: UIImageView!
+    
+    @IBOutlet var btnReszie: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+    }
+    
+    @IBAction func switchImageOnOff(_ sender: UISwitch) {
+    }
+    
 }
 
