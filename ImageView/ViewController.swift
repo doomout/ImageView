1553f280d322bd0403ac3b6419d2a78d8c29c43c
doomout <winquake3@hanmail.net>
2022-12-18 13:29:23 +0900

    Initial Commit

---
+++ ImageView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageView
+//
+//  Created by 현구김 on 2022/12/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
